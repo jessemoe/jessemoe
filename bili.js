92c5fc6d7a614e9f340a7d0a5b532b3e957749ee
Stark <43443357+jessemoe@users.noreply.github.com>
2024-06-13 21:14:25 +0400

    Update bili.js

--- bili.js
+++ bili.js
@@ -1,15 +1,18 @@
 /******************************
 
-[rewrite_local]
-
-
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/mosimoe/1/main/1.js
-
-[mitm]
-hostname = *.biliapi.*, *.bilibili.*
+#!name=bilibili自动
+#!desc=哔哩哔哩, 港澳台番剧自动切换地区 & 显示豆瓣评分  
+  
+  
+[MITM]  
+hostname = %APPEND% ap?.bili*i.com, ap?.bili*i.net  
+  
+[Script]  
+bili自动=type=http-request,pattern=^https:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/(pgc\/view\/v\d\/app\/season|x\/v\d\/search\/defaultwords) ,
+script-path=https://raw.githubusercontent.com/jessemoe/jessemoe/main/bili.js
 
 *******************************/
-
+// https://raw.githubusercontent.com/jessemoe/jessemoe/main/bili.js
 var modifiedHeaders=$request['headers'];
 modifiedHeaders['Referer']='https://www.bilibili.com/';
 modifiedHeaders['User-Agent']='bili-universal/71400100 os/ios model/iPhone 11 mobi_app/iphone osVer/15.3.1 network/2';
